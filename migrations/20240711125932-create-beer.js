505a6b900ddd7ab4a0b2bf43e8fe9d105707527e
Matthew Miner <misternuks@gmail.com>
2024-07-11 22:00:09 +0900

    create Beer model and migrate to the database

---
+++ migrations/20240711125932-create-beer.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Beers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      style: {
+        type: Sequelize.STRING
+      },
+      brewery: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      isNew: {
+        type: Sequelize.BOOLEAN
+      },
+      onSale: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Beers');
+  }
+};
